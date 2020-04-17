fac59ac5893932f7a9c0197a0ed2028afa477c4b
Shaoqing Tan <tansq7@gmail.com>
2020-04-17 00:40:33 -0400

    Add standalone mode for kut client

--- cmd/kut/main.go
+++ cmd/kut/main.go
@@ -16,42 +16,45 @@ func main() {
 		Name:    "kut",
 		Usage:   "run a kut executer.",
 		Version: version.Version,
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "master",
-				Value:   "http://localhost:7171",
-				Usage:   "use a different master than default, use cautiously!",
-				Aliases: []string{"m"},
-			},
-			&cli.StringFlag{
-				Name:    "channel",
-				Value:   "0",
-				Usage:   "select a channel.",
-				Aliases: []string{"c"},
-			},
-		},
-		Commands: []*cli.Command{
-			{
-				Name:    "start",
-				Usage:   "starts executor for a <menu>",
-				Aliases: []string{"s"},
-				Action: func(c *cli.Context) error {
-					if c.NArg() != 1 {
-						return cli.Exit("error: expect exactly 1 argument", -1)
-					}
+		Commands: append(
+			[]*cli.Command{
+				{
+					Name:    "start",
+					Usage:   "starts executor for a <menu>",
+					Aliases: []string{"s"},
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:    "master",
+							Value:   "http://localhost:7171",
+							Usage:   "use a different master than default, use cautiously!",
+							Aliases: []string{"m"},
+						},
+						&cli.StringFlag{
+							Name:    "channel",
+							Value:   "0",
+							Usage:   "select a channel.",
+							Aliases: []string{"c"},
+						},
+					},
+					Action: func(c *cli.Context) error {
+						if c.NArg() != 1 {
+							return cli.Exit("error: expect exactly 1 argument", -1)
+						}
 
-					master := c.String("master")
-					channel := c.String("channel")
-					menuName := c.Args().Get(0)
+						master := c.String("master")
+						channel := c.String("channel")
+						menuName := c.Args().Get(0)
 
-					if start(master, channel, menuName) != nil {
-						return cli.Exit("error: failed to start", -1)
-					}
+						if start(master, channel, menuName) != nil {
+							return cli.Exit("error: failed to start", -1)
+						}
 
-					return nil
+						return nil
+					},
 				},
 			},
-		},
+			commands()...,
+		),
 	}
 
 	app.Run(os.Args)
---
+++ cmd/kut/standalone.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/bettercallshao/kut/pkg/cmd"
+	"github.com/bettercallshao/kut/pkg/menu"
+	"github.com/urfave/cli/v2"
+)
+
+func commands() []*cli.Command {
+	commands := make([]*cli.Command, 0)
+
+	dryFlag := &cli.BoolFlag{
+		Name:  "dry",
+		Usage: "render command but don't run",
+	}
+
+	for _, name := range menu.List() {
+		menu, err := menu.Load(name)
+		if err != nil {
+			continue
+		}
+
+		subCommands := make([]*cli.Command, 0)
+		for _, action := range menu.Actions {
+
+			flags := make([]cli.Flag, 0)
+			for _, param := range action.Params {
+				flags = append(
+					flags,
+					&cli.StringFlag{
+						Name:  param.Name,
+						Value: param.Value,
+						Usage: param.Help,
+					},
+				)
+			}
+			flags = append(flags, dryFlag)
+
+			subCommands = append(
+				subCommands,
+				&cli.Command{
+					Name:   action.Name,
+					Usage:  action.Help,
+					Flags:  flags,
+					Action: act,
+				},
+			)
+		}
+
+		commands = append(
+			commands,
+			&cli.Command{
+				Name:        menu.Name,
+				Usage:       menu.Help,
+				Subcommands: subCommands,
+			},
+		)
+	}
+
+	return commands
+}
+
+func act(c *cli.Context) error {
+	dry := c.Bool("dry")
+
+	// the cli lib concatenates command hierarchy, menu name is the second
+	name := strings.Split(c.App.Name, " ")[1]
+
+	loaded, err := menu.Load(name)
+	if err != nil {
+		cli.Exit("failed to open menu "+name, -1)
+	}
+
+	action := menu.Action{}
+	for _, item := range loaded.Actions {
+		if item.Name == c.Command.Name {
+			action = item
+			break
+		}
+	}
+	if action.Name == "" {
+		cli.Exit("failed to find action "+c.Command.Name, -1)
+	}
+
+	for idx, param := range action.Params {
+		action.Params[idx].Value = c.String(param.Name)
+	}
+
+	input, err := menu.Render(action)
+	if err != nil {
+		cli.Exit(fmt.Sprintf("action failed to render %+v", action), -1)
+	}
+
+	if dry {
+		fmt.Printf("template: %s\n", action.Template)
+		fmt.Printf("rendered: %s\n", input)
+		return nil
+	}
+
+	output := make(chan cmd.Payload)
+	go cmd.Run(input, output)
+	for line := range output {
+		log.Printf("%6s %s", line.Pipe, line.Text)
+	}
+
+	return nil
+}
